92c17bca50559b9cbabd470771382601ac8b1443
Jennifer <jennifer.lay@psych.ubc.ca>
2016-12-05 21:54:52 -0800

    updated fbind

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,7 @@
+fbind <- function(a, b) {
+	factor(c(as.character(a), as.character(b)))
+}
+
 # (a <- factor(c("character", "hits", "your", "eyeballs")))
 # (b <- factor(c("but", "integer", "where it", "counts")))
 # c(a, b)
@@ -6,9 +10,5 @@
 # two factors to be an integer vector consisting of the numbers 1, 2, 3, and 4.
 
 # Here’s fbind(), a function that creates a new factor from two factors, a potentially less surprising result:
-
-fbind <- function(a, b) {
-	factor(c(as.character(a), as.character(b)))
-}
 
 # fbind(a,b)
